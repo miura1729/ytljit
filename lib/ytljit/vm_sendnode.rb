4628c95ae0a2d6a81810fa0f329479a09b7b072e
Miura Hideki <m-72@tf6.so-net.ne.jp>
2012-10-28 07:00:04 +0900

    Support registor Float variable for local variable assertion

--- lib/ytljit/vm_sendnode.rb
+++ lib/ytljit/vm_sendnode.rb
@@ -2388,16 +2388,19 @@ module YTLJit
           if rtype.ruby_type == Fixnum then
             asm = context.assembler
             if context.ret_reg.is_a?(OpRegistor) then
-              asm.with_retry do
-                asm.cvtsi2sd(XMM0, context.ret_reg)
+              if !context.ret_reg.is_a?(OpRegXMM) then
+                asm.with_retry do
+                  asm.cvtsi2sd(XMM0, context.ret_reg)
+                end
+                context.ret_reg = XMM0
               end
             else
               asm.with_retry do
                 asm.mov(TMPR, context.ret_reg)
                 asm.cvtsi2sd(XMM0, TMPR)
               end
+              context.ret_reg = XMM0
             end
-            context.ret_reg = XMM0
           end
           context = rtype.gen_unboxing(context)
           context = compile_main(context)
